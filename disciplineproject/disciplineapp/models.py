ed24e6c9a0e3a3803909a1b2a305572d47b77bb6
mirkezi <mirko.venali>
2024-07-04 23:30:21 +0200

    create index.html template, start admin page, improve visualization admin page

--- disciplineproject/disciplineapp/models.py
+++ disciplineproject/disciplineapp/models.py
@@ -3,8 +3,10 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
+    '''model for registered user'''
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
-        return f"Username: {User.username}, Password: {User.password}."
+        return f"Username: {User.username}, Password: {User.password}., Active: {User.is_active}"
